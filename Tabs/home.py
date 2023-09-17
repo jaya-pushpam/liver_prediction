0810b6c0f54cbefeedf1c2db389e43321a1702cf
jaya-pushpam <132914890+jaya-pushpam@users.noreply.github.com>
2023-09-17 14:05:02 +0530

    Update home.py

--- Tabs/home.py
+++ Tabs/home.py
@@ -15,5 +15,5 @@ def app():
     # Add brief describtion of your web app
     st.markdown(
     """<p style="font-size:20px;">
-            What is the main cause of liver disease?
-There are many types of liver disease, which can be caused by infections, inherited conditions, obesity and misuse of alcohol. Over time, liver disease may lead to scarring and more serious complications. Early treatment can help heal the damage and prevent liver failure.""", unsafe_allow_html=True)
+            What is the main cause of Hepatitis disease?
+There are many types of Hepatitis disease, which can be caused by infections, inherited conditions, obesity and misuse of alcohol. Over time, liver disease may lead to scarring and more serious complications. Early treatment can help heal the damage and prevent liver failure.""", unsafe_allow_html=True)
